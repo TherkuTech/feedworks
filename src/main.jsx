6eb87c8b82387f906bd00390233b3dbe1e8b4236
aswathcm29 <aswathcm29@gmail.com>
2024-05-24 00:31:29 +0530

    configured tailwind and kept routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import Login from './pages/LoginCred/Login.jsx'
+import Signup from './pages/LoginCred/Signup.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+  },
+
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    element:<Signup/>
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
